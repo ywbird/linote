26b602a3435c226114c395027b9ecfca9850fc11
ywbird <repack.lab.io@gmail.com>
2022-06-04 17:11:10 +0900

    init

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,6 +5,7 @@ const config: GatsbyConfig = {
     title: `test`,
     siteUrl: `https://ywbird.github.io`,
   },
+  pathPrefix: `/gatsby-site`,
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
